bac41e6e78e6745b5aa09c1db584d9911daaebd8
Bryan Ramaputra <ramaputra.bryan@gmail.com>
2023-02-21 11:24:54 -0800

    add simple scenario

--- test/specs/example.js
+++ test/specs/example.js
@@ -1,4 +1,9 @@
 describe('example test', function() {
     it('should run', async function() {
+        await $('~Login').click()
+        await $('~input-email').setValue('test@mail.com')
+        await $('~input-password').setValue('12345678')
+
+        await $('~You are logged in!').isDisplayed()
     })
 })
